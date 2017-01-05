3e1bef145363aee77d0946bc940a9449ae977284
子牙 <jiabin.cui@cmmbvision.com.cn>
2017-01-05 14:08:47 +0800

    add logback

--- lab-web/src/test/java/com/cuijb/web/test/LombokTest.java
+++ lab-web/src/test/java/com/cuijb/web/test/LombokTest.java
@@ -2,13 +2,18 @@ package com.cuijb.web.test;
 
 import com.cuijb.web.vo.Student;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class LombokTest {
 
 	public static void main(String[] args) {
+		log.debug("Student test start.");
 		Student st = new Student();
 		st.setName("Cuijb");
 		st.setSex("F");
 		st.setAge(18);
 		System.out.println(st);
+		log.debug("Student test end.");
 	}
 }
