78408fcc326f8a743a156dc30cf3688907e0eeb5
子牙 <jiabin.cui@cmmbvision.com.cn>
2017-01-05 14:51:58 +0800

    删除main方法

--- lab-web/src/test/java/com/cuijb/web/test/LombokTest.java
+++ lab-web/src/test/java/com/cuijb/web/test/LombokTest.java
@@ -9,16 +9,6 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class LombokTest {
 
-	public static void main(String[] args) {
-		log.debug("Student test start.");
-		Student st = new Student();
-		st.setName("Cuijb");
-		st.setSex("F");
-		st.setAge(18);
-		System.out.println(st);
-		log.debug("Student test end.");
-	}
-
 	@Test
 	public void junitTest() {
 		log.debug("Student junitTest start.");
