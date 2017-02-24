0e6421fa51b986071f7c3744c7c6fc43ac6807cd
子牙 <jiabin.cui@cmmbvision.com.cn>
2017-02-24 09:46:12 +0800

    SynchronizedMap Test

--- lab-web/src/test/java/com/cuijb/web/test/SyncTest.java
+++ lab-web/src/test/java/com/cuijb/web/test/SyncTest.java
@@ -1,6 +1,10 @@
 package com.cuijb.web.test;
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
@@ -38,5 +42,166 @@ public class SyncTest extends BaseTest {
 			}
 			// }
 		}).start();
+	}
+
+	@Test
+	public void syncMapTest() {
+		Map<Long, Boolean> map = Collections.synchronizedMap(new HashMap<>());
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				// synchronized (sortedSet) {
+				while (true) {
+					long sleep = random();
+					try {
+						Thread.sleep(sleep * 10);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					map.get(sleep);
+					map.get(sleep);
+					map.get(sleep);
+					map.get(sleep);
+					map.get(sleep);
+					map.get(sleep);
+					map.get(sleep);
+					map.get(sleep);
+					map.put(sleep, true);
+					map.put(sleep, true);
+					map.put(sleep, true);
+					map.put(sleep, true);
+					map.put(sleep, true);
+					map.put(sleep, true);
+					map.put(sleep, true);
+					map.put(sleep, true);
+					map.put(sleep, true);
+					System.out.println(sleep + " put ******");
+				}
+				// System.out.println(sortedSet);
+				// }
+			}
+		}).start();
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				// synchronized (sortedSet) {
+				while (true) {
+					long sleep = random();
+					try {
+						Thread.sleep(sleep * 10);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					map.get(sleep);
+					map.get(sleep);
+					map.get(sleep);
+					map.get(sleep);
+					map.get(sleep);
+					map.get(sleep);
+					map.get(sleep);
+					map.get(sleep);
+					map.put(sleep, true);
+					map.put(sleep, true);
+					map.put(sleep, true);
+					map.put(sleep, true);
+					map.put(sleep, true);
+					map.put(sleep, true);
+					map.put(sleep, true);
+					map.put(sleep, true);
+					map.put(sleep, true);
+					System.out.println(sleep + " put ------");
+				}
+				// System.out.println(sortedSet);
+				// }
+			}
+		}).start();
+		// new Thread(new Runnable() {
+		//
+		// @Override
+		// public void run() {
+		// while (true) {
+		// // synchronized (sortedSet) {
+		// long sleep = random();
+		// try {
+		// Thread.sleep(sleep * 1000);
+		// } catch (InterruptedException e) {
+		// e.printStackTrace();
+		// }
+		// map.put(sleep, false);
+		// // System.out.println(sortedSet);
+		// }
+		// }
+		// // }
+		// }).start();
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				while (true) {
+					long sleep = random();
+					try {
+						Thread.sleep(sleep * 1000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					synchronized (map) {
+
+						Iterator<Entry<Long, Boolean>> iter = map.entrySet().iterator();
+						while (iter.hasNext()) {
+							Entry<Long, Boolean> entry = iter.next();
+							if (entry.getValue()) {
+								System.out.println(entry.getKey() + " set false ******");
+								entry.setValue(false);
+							} else {
+								System.out.println(entry.getKey() + " remove ******");
+								iter.remove();
+							}
+						}
+					}
+				}
+			}
+			// }
+		}).start();
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				while (true) {
+					long sleep = random();
+					try {
+						Thread.sleep(sleep * 1000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					synchronized (map) {
+
+						Iterator<Entry<Long, Boolean>> iter = map.entrySet().iterator();
+						while (iter.hasNext()) {
+							Entry<Long, Boolean> entry = iter.next();
+							if (entry.getValue()) {
+								System.out.println(entry.getKey() + " set false ------");
+								entry.setValue(false);
+							} else {
+								System.out.println(entry.getKey() + " remove ------");
+								iter.remove();
+							}
+						}
+					}
+				}
+			}
+			// }
+		}).start();
+
+		try {
+			Thread.sleep(60 * 1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+		}
+	}
+
+	private long random() {
+		return Math.round(Math.random() * 4 + 1);
 	}
 }
